762e1edbc4cb8c9eab90144770d6f726363e99d9
Lildoza20 <lildozanmam@gmail.com>
2026-03-26 21:45:17 -0600

    Finished Both Programs Youtube Video | Online Ordering

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        List<Order> orders = new List<Order>();
+
+        Address address1 = new Address("742 Maple Street", "Phoenix", "AZ", "USA");
+        Customer customer1 = new Customer("John Doe", address1);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Wireless Mouse", "WM101", 24.99, 2));
+        order1.AddProduct(new Product("Mechanical Keyboard", "MK202", 70.50, 1));
+        order1.AddProduct(new Product("USB-C Cable", "UC303", 9.99, 3));
+        orders.Add(order1);
+
+        Address address2 = new Address("18 King Street West", "Toronto", "Ontario", "Canada");
+        Customer customer2 = new Customer("Jane Smith", address2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Notebook", "NB404", 4.50, 5));
+        order2.AddProduct(new Product("Desk Lamp", "DL505", 32.99, 1));
+        order2.AddProduct(new Product("Backpack", "BP606", 45.00, 1));
+        orders.Add(order2);
+
+        Address address3 = new Address("55 Oak Avenue", "Dallas", "TX", "United states");
+        Customer customer3 = new Customer("Michael Brown", address3);
+
+        Order order3 = new Order(customer3);
+        order3.AddProduct(new Product("Water Bottle", "WB707", 15.75, 2));
+        order3.AddProduct(new Product("Bluetooth Speaker", "BS808", 64.99, 1));
+        order3.AddProduct(new Product("Phone Charger", "PC909", 18.25, 2));
+        orders.Add(order3);
+
+        int orderNumber = 1;
+
+        foreach (Order order in orders)
+        {
+            Console.WriteLine($"Order #{orderNumber}");
+            Console.WriteLine(order.GetPackingLabel());
+            Console.WriteLine(order.GetShippingLabel());
+            Console.WriteLine($"Total Cost: ${order.GetTotalCost():0.00}");
+            orderNumber += 1;
+
+            Console.WriteLine();
+        }
     }
 }
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("I PLAYED SUPER BATTLE GOLF FOR THE FIRST TIME!", "MM7 Games", 1231);
+        video1.AddComment(new Comment("darkhonor13", "LOL, JJ and Harry in the thumbnail is mad. Get your views Simon!"));
+        video1.AddComment(new Comment("ethanquakenbush2477", "jj in the thumbnail is crazy"));
+        video1.AddComment(new Comment("alpheano", "Y'all should play it with rdc boys"));
+
+        Video video2 = new Video("This is what $1000 gets you in 2026!", "TechSource", 1502);
+        video2.AddComment(new Comment("Joeker8171", "I would love to see a build off $1,500 vs $1,500. Great idea"));
+        video2.AddComment(new Comment("christopherlegg5469", "A build off between you two would be cool I really wish micro center would open here in the UK"));
+        video2.AddComment(new Comment("DariansIncompeTech", "man, I wish I lived near a Micro Center"));
+
+        Video video3 = new Video("SIDEMEN BRUTALLY RANK UK FAST FOOD CHAINS", "SidemenReacts", 1507);
+        video3.AddComment(new Comment("arin9953", "These four carrying this channel"));
+        video3.AddComment(new Comment("Tom-Nr1", "WEE NEED A SIDEMEN VIDEO WHERE U GO TO THE FOOD CHAINS U HAVENT BEEN TO!!!!"));
+        video3.AddComment(new Comment("juiced9432", "Simon putting Greggs in the highest tier sums up his food tastes"));
+
+        videos.Add(video1);
+        videos.Add(video2);
+        videos.Add(video3);
+
+        foreach (Video video in videos)
+        {
+            Console.WriteLine($"Title: {video.GetTitle()}");
+            Console.WriteLine($"Author: {video.GetAuthor()}");
+            Console.WriteLine($"Length: {video.GetLengthInSeconds()} seconds");
+            Console.WriteLine($"Number of Comments: {video.GetCommentCount()}");
+            Console.WriteLine("Comments: ");
+
+            foreach (Comment comment in video.GetComments())
+            {
+                Console.WriteLine($"- {comment.GetName()}: {comment.GetText()}");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
